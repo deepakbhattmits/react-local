84489605cc7c9c47c99c327fbf9a8fffa2bf49bc
deepak bhatt <deepakbhattmits@gmail.com>
2019-11-23 13:31:53 +0530

    new modification

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ScrollApp from './scroll';
 import logo from '../assets/images/logo.svg';
 import '../assets/css/App.css';
 
 
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App-header">
-        <header className="">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Deepak          </p>
-            <p>
-            Deepak          </p> <p>
-            Deepak          </p>
-            <p>
-            Deepak          </p>
-            <p>
-            Deepak          </p>
-            <p>
-            Deepak          </p>
-            <p>
-            Deepak          </p>
-            <p>
-            Deepak          </p>
-            <p>
-            Deepak          </p>
-            <p>
-            Deepak          </p>
-            
-          <ScrollApp />
-        </header>
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div className="App-header">
+      <header className="">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Deepak</p>
+        <p>Deepak</p>
+        <p>Deepak</p>
+        <p>Deepak</p>
+        <p>Deepak</p>
+        <p>Deepak</p>
+        <p>Deepak</p>
+        <p>Deepak</p>
+        <p>Deepak</p>
+        <p>Deepak</p>
+        <ScrollApp />
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,7 @@ import './assets/css/index.css';
 import './assets/css/App.css';
 import './assets/less/custom.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css'
-import App from './components/App'; 
-import * as serviceWorker from './serviceWorker';
+import App from './components/App';
+const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, rootElement);
