1e57e1b10e2dc845c7e6da4f19c778ef333f2475
deepak bhatt <deepakbhattmits@gmail.com>
2019-11-23 19:09:38 +0530

    bootstrap variables override

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import ScrollApp from './scroll';
-import logo from '../assets/images/logo.svg';
-import '../assets/css/App.css';
-
-
-
+import ScrollApp from './ScrollApp';
+// import GoTop from './GoTop';
+import { ReactComponent as SVGLogo } from '../assets/images/logo.svg';
+import Form from './Form'
 const App = () => {
   return (
     <div className="App-header">
       <header className="">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Deepak</p>
-        <p>Deepak</p>
+        <SVGLogo className='App-logo' />
+        <div className='alert alert-primary'>Deepak</div>
+        <div className='alert alert-danger'>Deepak</div>
+        <Form />
         <p>Deepak</p>
         <p>Deepak</p>
         <p>Deepak</p>
@@ -21,6 +20,7 @@ const App = () => {
         <p>Deepak</p>
         <p>Deepak</p>
         <ScrollApp />
+        {/* <GoTop /> */}
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/css/index.css';
 import './assets/css/App.css';
-import './assets/less/custom.css';
+import './assets/less/custom.less';
+import './assets/scss/main.scss'
 import '../node_modules/font-awesome/css/font-awesome.min.css'
 import App from './components/App';
 const rootElement = document.querySelector('#root');
