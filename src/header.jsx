4fa9c722efcdbf1d8e45d6b101e65737866b02dc
deepak bhatt <deepakbhattmits@gmail.com>
2019-11-24 10:21:28 +0530

    refector navigation

--- src/header.jsx
+++ src/header.jsx
@@ -1,36 +1,13 @@
 import React from 'react';
 import ScrollApp from './components/ScrollApp';
-import { Navbar, Nav, NavDropdown } from "react-bootstrap";
-
+import Navigation from './components/Navigation';
 
 
 const Header = () => {
 
   return (
     <>
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand href="#home">Brand</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-      </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <Navigation />
       <ScrollApp />
     </>
   );
